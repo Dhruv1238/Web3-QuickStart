85ce5cf36e4c736865becfc1bda7ead673fadb92
Dhruv1238 <dhruv4075@gmail.com>
2023-10-04 17:56:34 +0530

    Fixed Client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Button, Card, Typography } from '@material-tailwind/react'
+import { Typography } from '@material-tailwind/react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      {/* <h1>Vite + React</h1> */}
-      <Typography color="blue" className="text-5xl">Vite + React</Typography>
-      <Card className=' w-1/5'>
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        <Button onClick={() => setCount((count) => count + 1)} color='purple'>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </Card>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Typography color="blue" className='flex items-center justify-self-center'>Hello World</Typography>
     </>
   )
 }
